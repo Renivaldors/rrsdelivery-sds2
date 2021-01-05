3ae26fd17629b0ed1696d1a24f02d27ea7f6c993
Renivaldo Rodrigues dos Santos <renivaldor@hotmail.com>
2021-01-04 21:29:47 -0300

    Project crreated

---
+++ backend/src/main/java/com/devsuperior/rrsdelivery/RrsdeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.rrsdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrsdeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrsdeliveryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/rrsdelivery/RrsdeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.rrsdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrsdeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
