d90140da227c22e996dce5864c5415ea48aece95
Renivaldo Rodrigues dos Santos <renivaldor@hotmail.com>
2021-01-08 20:23:31 -0300

    Add Home Page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import Home from './Home';
 import Navbar from './NavBar';
 
 function App() {
   return (
-    <div >
+    <>
       <Navbar />
-    </div>
+      <Home />
+    </>
   );
 }
 
